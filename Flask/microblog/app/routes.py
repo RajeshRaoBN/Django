21a572d99c9700221f523e10a4c52e3a033066b9
Rajesh Narayana Rao <mailrbn@gmail.com>
2018-11-11 14:03:56 +1100

    11/11/2018

---
+++ Flask/microblog/app/routes.py
@@ -0,0 +1,28 @@
+from flask import render_template
+from app import app
+from app.forms import LoginForm
+
+# ...
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello, World!"
+@app.route('/rajesh')
+def rajesh():
+    user = {'username': 'Rajesh'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='', user=user, posts=posts)
